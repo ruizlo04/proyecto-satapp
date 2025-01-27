c2f9a6950a96f84536d23cd6c1bb2883cdcce60c
Carlos ruiz <ruiz.locar23@triana.salesianos.edu>
2025-01-27 13:38:42 +0100

    project upload

---
+++ proyecto_satapp_Carlos_Rafa/src/main/java/com/example/proyecto_satapp_Carlos_Rafa/ProyectoSatappCarlosRafaApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_satapp_Carlos_Rafa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSatappCarlosRafaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSatappCarlosRafaApplication.class, args);
+	}
+
+}
---
+++ proyecto_satapp_Carlos_Rafa/src/test/java/com/example/proyecto_satapp_Carlos_Rafa/ProyectoSatappCarlosRafaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_satapp_Carlos_Rafa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSatappCarlosRafaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
