df2784133fe2f9b3437fa8f6372e9ccf92b77666
Carlos ruiz <ruiz.locar23@triana.salesianos.edu>
2025-01-29 12:19:09 +0100

    add some inserts into

--- proyecto_satapp_Carlos_Rafa/src/main/resources/import.sql
+++ proyecto_satapp_Carlos_Rafa/src/main/resources/import.sql
@@ -1,1 +1,5 @@
 INSERT INTO usuario  (id, username) VALUES (nextval ('usuario_seq'), 'Rafahm03');
+
+INSERT INTO incidencias (id, titulo, urgencia) VALUES (nextval('incidencias_seq'), 'incidencia1', true);
+
+INSERT INTO ubicaciones (id, nombre) VALUES (nextval('ubicaciones_seq'), 'Ubicacion1');
