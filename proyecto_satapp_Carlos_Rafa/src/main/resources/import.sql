6acaeebff624db36741567f18c1859f9152d4a3b
ruizlo04 <carlo@CalosRuizLopez>
2025-01-30 19:46:03 +0100

    assosiations between equipo and incidencia correct

--- proyecto_satapp_Carlos_Rafa/src/main/resources/import.sql
+++ proyecto_satapp_Carlos_Rafa/src/main/resources/import.sql
@@ -3,9 +3,12 @@ INSERT INTO usuario (id, username) VALUES (nextval ('usuario_seq'), 'RuizloCar')
 
 INSERT INTO ubicaciones (id, nombre) VALUES (nextval('ubicaciones_seq'), 'Ubicacion1');
 INSERT INTO equipos (id, nombre, caracteristicas) VALUES (nextval('equipos_seq'), 'Equipo1', '["Característica 1", "Característica 2", "Característica 3"]');
+INSERT INTO equipos (id, nombre, caracteristicas) VALUES (nextval('equipos_seq'), 'Equipo2', '["Característica 1", "Característica 2", "Característica 3"]');
 
 INSERT INTO incidencias (id, fecha_incidencia, titulo, descripcion, urgencia, ubicacion_id, estado, equipo_id)
 VALUES (nextval('incidencias_seq'), '2025-01-29 13:10:00' ,'incidencia1', 'esta incidencia es de prueba',true, 1, 'ABIERTA', 1);
+INSERT INTO incidencias (id, fecha_incidencia, titulo, descripcion, urgencia, ubicacion_id, estado, equipo_id)
+VALUES (nextval('incidencias_seq'), '2025-01-29 13:10:00' ,'incidencia2', 'esta incidencia es de prueba',true, 1, 'ABIERTA', 51);
 
 INSERT INTO alumno (id) VALUES (1);
 
