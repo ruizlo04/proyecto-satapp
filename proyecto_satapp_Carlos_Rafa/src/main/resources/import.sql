b27db4d469b0f2cd9c4ed4a908fc5cc65f7c35e3
Carlos ruiz <ruiz.locar23@triana.salesianos.edu>
2025-02-05 09:36:09 +0100

    postgresql add to project

--- proyecto_satapp_Carlos_Rafa/src/main/resources/import.sql
+++ proyecto_satapp_Carlos_Rafa/src/main/resources/import.sql
@@ -6,9 +6,14 @@ INSERT INTO usuario (id, username) VALUES (nextval ('usuario_seq'), 'User12345')
 INSERT INTO ubicaciones (id, nombre) VALUES (nextval('ubicaciones_seq'), 'Ubicacion1');
 INSERT INTO ubicaciones (id, nombre) VALUES (nextval('ubicaciones_seq'), 'Ubicacion2');
 
-INSERT INTO equipos (id, nombre, caracteristicas, ubicacion_id) VALUES (nextval('equipos_seq'), 'Equipo1', '["Característica 1", "Característica 2", "Característica 3"]', 1);
-INSERT INTO equipos (id, nombre, caracteristicas, ubicacion_id) VALUES (nextval('equipos_seq'), 'Equipo2', '["Característica 1", "Característica 2", "Característica 3"]', 1);
-INSERT INTO equipos (id, nombre, caracteristicas, ubicacion_id) VALUES (nextval('equipos_seq'), 'Equipo3', '["Característica 1", "Característica 2", "Característica 3"]', 51);
+INSERT INTO equipos (id, nombre, caracteristicas, ubicacion_id)
+VALUES (nextval('equipos_seq'), 'Equipo1', ARRAY['Característica 1', 'Característica 2', 'Característica 3'], 1);
+
+INSERT INTO equipos (id, nombre, caracteristicas, ubicacion_id)
+VALUES (nextval('equipos_seq'), 'Equipo2', ARRAY['Característica 1', 'Característica 2', 'Característica 3'], 1);
+
+INSERT INTO equipos (id, nombre, caracteristicas, ubicacion_id)
+VALUES (nextval('equipos_seq'), 'Equipo3', ARRAY['Característica 1', 'Característica 2', 'Característica 3'], 51);
 
 INSERT INTO incidencias (id, fecha_incidencia, titulo, descripcion, urgencia, ubicacion_id, estado, equipo_id, usuario_id)
 VALUES (nextval('incidencias_seq'), '2025-01-29 13:10:00' ,'incidencia1', 'esta incidencia es de prueba',true, 1, 'ABIERTA', 1, 1);
